ce16c87774d787ff5de6e2e8ee20ee8f2c7767c5
delattre1 <daniieeldelattre@gmail.com>
2021-01-24 23:47:50 -0300

    can access mongodb

--- upload/app.py
+++ upload/app.py
@@ -10,8 +10,18 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = save_path
 
 
+def get_credentials():
+    import json
+
+    with open('.credentials.txt') as json_file:
+        data = json.load(json_file)
+        database_credential = data['database']
+    return database_credential
+
+
 @app.route('/')
 def main():
+    da = get_credentials()
     return render_template('upload.html')
 
 
@@ -30,6 +40,29 @@ def show_boleto_name():
     data, vencimento, codigo = get_data_from_boleto(path)
     dados = data + ' ' + vencimento
     return dados
+
+
+@app.route('/see_mongo')
+def see_mongo():
+
+    import pymongo
+    db_credential = get_credentials()
+    client = pymongo.MongoClient(db_credential)
+    db = client['teste']
+    collection = db['collection_teste']
+    tudo = collection.find({})
+    v1 = tudo[0]['valor']
+    return str(v1)
+
+
+@app.route('/contas')
+def show_contas():
+    import pymongo
+    client = pymongo.MongoClient(db_credential)
+    db = client['teste']
+    collection = db['collection_teste']
+    boletos = collection.find({})
+    return render_template('contas.html', boletos=boletos)
 
 
 if __name__ == '__main__':
---
+++ upload/t1mongo.py
@@ -0,0 +1,5 @@
+import pymongo 
+
+client = pymongo.MongoClient("mongodb+srv://daniel:D4n13l_*@cluster0.iuofc.mongodb.net/boletos?retryWrites=true&w=majority")
+
+
